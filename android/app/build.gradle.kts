937ad1deaf96fc86c116ccf0b44d81f723f42377
Yousf Karim <yousfkarim2001@users.noreply.github.com>
2026-07-24 17:35:53 +0300

    Update SDK target to 36, version 2.0.8+201

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     // ndkVersion = "27.0.12077973"
     ndkVersion = "29.0.14206865"
     namespace = "com.khatmah.quran.yusf.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
 
 
     compileOptions {
@@ -34,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
